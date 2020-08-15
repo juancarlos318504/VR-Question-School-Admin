5f9ee3498db5a098aa19b090b25c4846b903ae3a
JuanCarlos <juancarlos318504@gmail.com>
2020-08-14 20:01:40 -0500

    Updates

---
+++ precache-manifest.ac20d2c184a55de9ccaad09cf781e9de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836cb9a444e29475fb057fa746a99356",
+    "url": "/VR-Question-School-Admin/index.html"
+  },
+  {
+    "revision": "116aa15c92f24f3d2e77",
+    "url": "/VR-Question-School-Admin/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c5169a06a16d5f1d1bba",
+    "url": "/VR-Question-School-Admin/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "116aa15c92f24f3d2e77",
+    "url": "/VR-Question-School-Admin/static/js/2.862e4e16.chunk.js"
+  },
+  {
+    "revision": "83cf22677282ef599fd4eb17fb28d692",
+    "url": "/VR-Question-School-Admin/static/js/2.862e4e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5169a06a16d5f1d1bba",
+    "url": "/VR-Question-School-Admin/static/js/main.1f73daca.chunk.js"
+  },
+  {
+    "revision": "61d04293d3cb298f8557",
+    "url": "/VR-Question-School-Admin/static/js/runtime-main.096e82ec.js"
+  }
+]);
